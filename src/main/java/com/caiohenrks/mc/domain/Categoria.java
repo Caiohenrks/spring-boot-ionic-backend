c0f77e0834d562ee818965eaafc52000a65cd1ee
Caio Henrique <caiohenrks@outlook.com>
2021-09-29 13:59:40 -0300

    criando repository e service para categoria

--- src/main/java/com/caiohenrks/mc/domain/Categoria.java
+++ src/main/java/com/caiohenrks/mc/domain/Categoria.java
@@ -3,11 +3,17 @@ package com.caiohenrks.mc.domain;
 
 import java.io.Serializable;
 import java.util.Objects;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
+@Entity
 public class Categoria implements Serializable{
     public static final long serialVersionUID = 1L;
-
     
+    @Id
+    @GeneratedValue(strategy=GenerationType.IDENTITY)
     private Integer id;
     private String nome;
 
---
+++ src/main/java/com/caiohenrks/mc/repositories/CategoriaRepository.java
@@ -0,0 +1,10 @@
+package com.caiohenrks.mc.repositories;
+
+import com.caiohenrks.mc.domain.Categoria;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
+
+}
